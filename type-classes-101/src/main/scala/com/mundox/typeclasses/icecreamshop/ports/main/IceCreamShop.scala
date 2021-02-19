4cceae4b70c88a7cb00475d353f180bcfeff0743
Milenio X <jnkmiloq@gmail.com>
2021-02-18 21:59:53 -0500

    Main class to start the Ice Cream Shop

---
+++ type-classes-101/src/main/scala/com/mundox/typeclasses/icecreamshop/ports/main/IceCreamShop.scala
@@ -0,0 +1,12 @@
+package com.mundox.typeclasses.icecreamshop.ports.main
+
+import com.mundox.typeclasses.icecreamshop.ports.adapters.IceCreamMachineAdapter
+import com.mundox.typeclasses.icecreamshop.ports.ui.model.DTODummies
+
+object IceCreamShop extends App {
+
+  println(s"Welcome to our Ice Cream Store")
+  println(s"So, your order is a Chocolate with Vanilla Ice Cream and with a Cookie, please hold on...")
+  val iceCreamRequested = DTODummies.buildChocolateVainillaCookieIceCream()
+  val iceCreamMachineService = new IceCreamMachineAdapter
+}
